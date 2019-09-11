fefbdb4c8e923212a56b3c6e520895d9c37f0b48
Andrés Villacob <avillacobh@unal.edu.co>
2019-09-11 12:57:05 -0500

    serie del seno

---
+++ 2019-09-11-errors/serie.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <cmath>
+
+
+
+
+int main (void)
+{
+  int N =50;
+  double x =0.01;
+  
+  std::ofstream fout ("datos.txt");
+
+  for(x = 0.1; x< 8.0; x += 0.1){
+    double term = x;
+    double sum = x;
+    
+    for (int ii = 2; ii <= N; ++ii)
+      {
+	term = term*(-1)*x*x/((2*ii)*(2*ii+1));
+	sum += term;
+      }
+
+    fout << x << "\t" << term << "\t" << sum << "\t" << std::sin(x) << "\n";
+  }
+  fout.close();
+  
+  return 0;
+}
+
+
