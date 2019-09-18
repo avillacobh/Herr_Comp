5898ccf4ecabe496c3047c6990bae1122a02fb7c
Andrés Villacob <avillacobh@unal.edu.co>
2019-09-18 11:30:40 -0500

    errores de resta

---
+++ 2019-09-11-errors/2019-09-18-errors2/error.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <cmath>
+
+int main (void)
+{
+  int max = 100;
+  
+  float a = 1.0 ,b= 1.0, c =0.0;
+  float x1=0.0, x2 = 0.0, x3 = 0.0, x4=0.0 ;
+
+  
+  for (int i = 1; i <= max; i++)
+    {
+      c= std::pow(10.0, -i);
+      float det = std::sqrt (b*b - 4*a*c);
+      x1 = (-b + det)/(2*a);
+      x2 = (-b - det)/(2*a);
+      x3 = (-2*c)/(b + det);
+      x4 = (-2*c)/(b - det);
+
+      std::printf("%10d %10.6f %10.6f  %10.6f %10.6f\n",i, x1, x2, x3, x4);
+    }
+  return 0;
+}
