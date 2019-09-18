82f7264553a188ddeb636810c6bb44a7005f848f
Andrés Villacob <avillacobh@unal.edu.co>
2019-09-18 12:37:10 -0500

    Errores de resta en la suma

--- 2019-09-11-errors/2019-09-18-errors2/error2.cpp
+++ 2019-09-11-errors/2019-09-18-errors2/error2.cpp
@@ -7,13 +7,13 @@ float sum3 (int max);
 
 int main (void)
 {
-  int nmax =10;
+  int nmax =10000;
   for (int i = 1; i<= nmax; i++)
     {
       float s3 = sum3(i);
       float delta1 = std::abs((sum1(i)-s3)/s3);
       float delta2 = std::abs((sum2(i)-s3)/s3);
-      std::printf ("%10d %10f %10f %10f %10f %10f \n",i,sum1(i),sum2(i),sum3(i), delta1, delta2);
+      std::printf ("%10d %10e %10e %10e %10e %10e \n",i,sum1(i),sum2(i),sum3(i), delta1, delta2);
     }
 
   return 0;
