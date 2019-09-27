6797a4fdcca897033ba640136ba3d81ed78822d8
Andrés Villacob <avillacobh@unal.edu.co>
2019-09-27 13:00:54 -0500

    Genera números aleatorio

---
+++ 2019-09-27-StandardLibrary/random.cpp
@@ -0,0 +1,31 @@
+#include <random>
+#include <iostream>
+#include <vector>
+ 
+int main()
+{
+  const int NSAMPLES = 1e8;
+  int seed = 1;
+  
+  const double xmin =0.0;
+  const double xmax = 1.0;
+  const double dx = 0.01;
+  const int nbins = (xmax-xmin)/dx;
+  
+  std::vector<double> histo(nbins);
+  
+  std::mt19937 gen(seed); //Standard mersenne_twister_engine seeded with seed
+  std::uniform_real_distribution<> dis(xmin, xmax);
+
+  for (int n = 0; n < NSAMPLES; ++n) {
+    double r = dis(gen);
+    int idx = int (r/dx);
+    histo[idx] += 1;
+  }
+
+  for(int i = 0; i < nbins; i++){
+    std::cout << xmin + i*dx << "\t" << histo[i]/(NSAMPLES*dx) << "\n";
+  }
+  
+  return 0;  
+}
